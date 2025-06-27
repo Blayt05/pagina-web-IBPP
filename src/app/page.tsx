338e3b0b0c08dbcd23f56bb05b378ebecbe2d39a
Israel Booz <A00840268@tec.mx>
2025-06-27 00:25:15 -0600

    Subiendo Main Page Celular

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,58 @@
+"use client"
 import Image from "next/image";
+import { useState, useRef } from "react";
 
 export default function Home() {
+
+  const slides = [
+    {
+      title: "Luz y verdad",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.",
+      author: "By Mathew Johnson",
+      date: "Tuesday 13 May, 2021",
+    },
+    {
+      title: "Camino vivo",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.",
+      author: "By Mathew Johnson",
+      date: "Tuesday 13 May, 2021",
+    },
+    {
+      title: "Firme en fe",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.",
+      author: "By Mathew Johnson",
+      date: "Tuesday 13 May, 2021",
+    },
+    {
+      title: "Roca eterna",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.",
+      author: "By Mathew Johnson",
+      date: "Tuesday 13 May, 2021",
+    },
+  ];
+
+  const [current, setCurrent] = useState(0);
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  const goToSlide = (idx: number) => {
+    setCurrent(idx);
+    const carousel = carouselRef.current;
+    if (carousel) {
+      const slide = carousel.children[idx] as HTMLElement;
+      carousel.scrollTo({ left: slide.offsetLeft, behavior: "smooth" });
+    }
+  };
+
+  // Update current slide on scroll
+  const handleScroll = () => {
+    const carousel = carouselRef.current;
+    if (carousel) {
+      const { scrollLeft, offsetWidth } = carousel;
+      const idx = Math.round(scrollLeft / offsetWidth);
+      setCurrent(idx);
+    }
+  };
+
   return (
     <main >
 
@@ -28,7 +80,6 @@ export default function Home() {
           </div> 
         </div>
       </div>
-
 
       {/* Iglesia con Vision */}
       <div className="text-center text-3xl pt-20 pb-20">
@@ -106,31 +157,50 @@ export default function Home() {
           Quae earum asperiores libero officia non eius unde odit quidem atque sunt!
         </p>
         
-        <div className="p-5">
-          <Image
-            src="/images/Pastor IBPP 1.svg"
-            alt="Imagen de Pastor"
-            width={500}
-            height={500}
-            className="rounded-4xl pb-3"
-          />
-
-          <Image
-            src="/images/Chicas Orando IBPP 2.svg"
-            alt="Imagen de Chicas IBPP"
-            width={500}
-            height={500}
-            className="rounded-4xl pb-3"
-          />
-
-          <Image
-            src="/images/Hermanos Orando IBPP 1.svg"
-            alt="Imagen de Chicas IBPP"
-            width={500}
-            height={500}
-            className="rounded-4xl pb-3"
-          />
-
+      
+        <div className=" carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full flex flex-col items-center">
+            <Image
+              src="/images/Pastor IBPP 1.svg"
+              alt="Imagen de Pastor"
+              width={500}
+              height={500}
+              className="rounded-4xl pb-3"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide3" className="btn btn-circle">❮</a>
+              <a href="#slide2" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          
+          <div id="slide2" className="carousel-item relative w-full flex flex-col items-center">
+            <Image
+              src="/images/Chicas Orando IBPP 2.svg"
+              alt="Imagen de Chicas IBPP"
+              width={500}
+              height={500}
+              className="rounded-4xl pb-3"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide1" className="btn btn-circle">❮</a>
+              <a href="#slide3" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full flex flex-col items-center">
+            <Image
+              src="/images/Hermanos Orando IBPP 1.svg"
+              alt="Imagen de Chicas IBPP"
+              width={500}
+              height={500}
+              className="rounded-4xl pb-3"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide2" className="btn btn-circle">❮</a>
+              <a href="#slide1" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          
+        </div>
 
           <h2 className="font-semibold text-3xl pt-10">Celebra con Nosotros</h2>
           <p className="text-center text-xl pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -146,80 +216,78 @@ export default function Home() {
               width={50}
               height={50}
             />
-          </div>
         </div>
       </div>
       
       {/* Beneficios de Unirte a la Iglesia */}
       <div>
         <h1 className="text-center text-3xl font-bold pb-5">Los beneficios de unirte a nuestra iglesia</h1>
-        
-        <div className="relative p-5">
-          <Image
-            src="/images/LOGO IBPP.svg"
-            alt="=Logo de IBPP"
-            width={800}
-            height={500}
-          />
-          <div className="absolute inset-0 pt-90 p-5">
-            <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
-              quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
-              similique cupiditate!</p>
+
+        <div className="carousel carousel-vertical rounded-box h-145">
+          <div className="carousel-item relative p-5">
+            <Image
+              src="/images/LOGO IBPP.svg"
+              alt="=Logo de IBPP"
+              width={800}
+              height={500}
+            />
+            <div className="absolute inset-0 pt-90 p-5">
+              <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
+                quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
+                similique cupiditate!</p>
+            </div>
           </div>
-          
-        </div>
 
 
-        <div className="relative p-5">
-          <Image
-            src="/images/LOGO IBPP.svg"
-            alt="=Logo de IBPP"
-            width={800}
-            height={500}
-          />
-          <div className="absolute inset-0 pt-90 p-5">
-            <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
-              quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
-              similique cupiditate!</p>
+          <div className="carousel-item relative p-5">
+            <Image
+              src="/images/LOGO IBPP.svg"
+              alt="=Logo de IBPP"
+              width={800}
+              height={500}
+            />
+            <div className="absolute inset-0 pt-90 p-5">
+              <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
+                quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
+                similique cupiditate!</p>
+            </div>
           </div>
-          
-        </div>
 
-        <div className="relative p-5">
-          <Image
-            src="/images/LOGO IBPP.svg"
-            alt="=Logo de IBPP"
-            width={800}
-            height={500}
-          />
-          <div className="absolute inset-0 pt-90 p-5">
-            <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
-              quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
-              similique cupiditate!</p>
+          <div className=" carousel-item relative p-5">
+            <Image
+              src="/images/LOGO IBPP.svg"
+              alt="=Logo de IBPP"
+              width={800}
+              height={500}
+            />
+            <div className="absolute inset-0 pt-90 p-5">
+              <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
+                quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
+                similique cupiditate!</p>
+            </div>
           </div>
-          
-        </div>
 
-        <div className="relative p-5">
-          <Image
-            src="/images/LOGO IBPP.svg"
-            alt="=Logo de IBPP"
-            width={800}
-            height={500}
-          />
-          <div className="absolute inset-0 pt-90 p-5">
-            <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
-              quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
-              similique cupiditate!</p>
-          </div>
+            <div className=" carousel-item relative p-5">
+              <Image
+                src="/images/LOGO IBPP.svg"
+                alt="=Logo de IBPP"
+                width={800}
+                height={500}
+              />
+              <div className="absolute inset-0 pt-90 p-5">
+                <h2 className="font-semibold text-xl">Watch and listen to our sermons</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Quas, temporibus. Fugit ullam assumenda possimus esse expedita,
+                  quas dignissimos in ea, facere vel repellendus qui at dolorum id repudiandae,
+                  similique cupiditate!</p>
+              </div>
+            </div>
         </div>
       </div>
 
@@ -295,53 +363,40 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Seccion de Compartir, Inspirar y innovar */}
+
       <div className="p-10">
-        <h2 className="text-center font-bold text-4xl">Compartir, inspirar y innovar</h2>
-
-        <div className="pt-5">
-          <div className="bg-[#FFF5EB] border-[#FFD0A0] border-b-10 rounded-2xl p-5">
-            <h2 className="font-bold text-xl pb-3">Luz y verdad</h2>
-            <p className="pb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-              Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.
-            </p>
-            <p>By Mathew Johnson</p>
-            <p>Tuesday 13 May, 2021</p>
-          </div>
+        <h2 className="text-center font-bold text-4xl pb-10">Compartir, inspirar y innovar</h2>
+        <div
+          className="flex overflow-x-auto gap-4 snap-x snap-mandatory pb-4 scroll-smooth"
+          ref={carouselRef}
+          onScroll={handleScroll}
+          style={{ scrollSnapType: "x mandatory" }}
+        >
+          {slides.map((slide, idx) => (
+            <div
+              key={idx}
+              className="min-w-full md:min-w-[400px] max-w-md flex-shrink-0 snap-center w-full bg-amber-200 p-3 rounded-2xl"
+            >
+              <div className="bg-[#FFF5EB] border-[#FFD0A0] border-b-10 rounded-2xl p-5 h-full">
+                <h2 className="font-bold text-xl pb-3">{slide.title}</h2>
+                <p className="pb-5">{slide.text}</p>
+                <p>{slide.author}</p>
+                <p>{slide.date}</p>
+              </div>
+            </div>
+          ))}
         </div>
-
-
-        <div className="pt-5">
-          <div className="bg-[#FFF5EB] border-[#FFD0A0] border-b-10 rounded-2xl p-5">
-            <h2 className="font-bold text-xl pb-3">Camino vivo</h2>
-            <p className="pb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-              Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.
-            </p>
-            <p>By Mathew Johnson</p>
-            <p>Tuesday 13 May, 2021</p>
-          </div>
-        </div>
-
-        <div className="pt-5">
-          <div className="bg-[#FFF5EB] border-[#FFD0A0] border-b-10 rounded-2xl p-5">
-            <h2 className="font-bold text-xl pb-3">Firme en fe</h2>
-            <p className="pb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-              Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.
-            </p>
-            <p>By Mathew Johnson</p>
-            <p>Tuesday 13 May, 2021</p>
-          </div>
-        </div>
-
-        <div className="pt-5">
-          <div className="bg-[#FFF5EB] border-[#FFD0A0] border-b-10 rounded-2xl p-5">
-            <h2 className="font-bold text-xl pb-3">Roca eterna</h2>
-            <p className="pb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-              Dolore, voluptatem aut! Beatae sit fugit rem necessitatibus.
-            </p>
-            <p>By Mathew Johnson</p>
-            <p>Tuesday 13 May, 2021</p>
-
-          </div>
+        {/* Dots */}
+        <div className="flex justify-center gap-2 mt-4">
+          {slides.map((_, idx) => (
+            <button
+              key={idx}
+              className={`w-3 h-3 rounded-full ${current === idx ? "bg-amber-500" : "bg-amber-200"} transition-colors`}
+              onClick={() => goToSlide(idx)}
+              aria-label={`Ir al slide ${idx + 1}`}
+            />
+          ))}
         </div>
       </div>
 
